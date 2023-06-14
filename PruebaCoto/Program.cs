7b2f8be12bf7d3b6716e8ef762ca02d7a8cfb7d5
Monesterolo Pedro <peedromonesterolo@gmail.com>
2023-06-14 15:30:46 -0300

    Inicio de proyecto con capas API, CORE e INFRASTRUCTURE + Inicio de proyecto con capas: -API (Disponibilidad de endpoinds de Venta, Centro de Distribucion y Modelos, Inyeccion de dependencias en program.cs) -CORE (Interfaces y Entidades) -INFRASTRUCTURE (Repositorios)

---
+++ PruebaCoto/Program.cs
@@ -0,0 +1,34 @@
+using API.Extensions;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Automapper para DTO a Entidades
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAplicacionServices();
+builder.Services.ConfigureServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
